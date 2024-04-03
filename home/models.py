a4161420f411e7b084b0d9e99455fa5ca147c0d7
Arman-Amini369 <arman.369.amini@gmail.com>
2024-04-03 03:43:49 +0330

    testing the pycharm and coudn't work with that due to my laptop's RAM

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=100)
@@ -10,10 +11,8 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-
     class Meta:
         ordering = ["-updated", "-created"]
-    
 
     def __str__(self):
         return self.title
@@ -31,6 +30,7 @@ class Post(models.Model):
         else:
             return False
 
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ucomments")
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="pcomments")
@@ -41,7 +41,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.user} in {self.post} have commented {self.body[:30]}"
-    
+
 
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="uvotes")
